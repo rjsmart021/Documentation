5acaf93f9466332c37422da97967d0a6c17d2b58
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-27 00:01:02 -0600

    Update productBP.py

--- Factory Management/routes/productBP.py
+++ Factory Management/routes/productBP.py
@@ -3,5 +3,6 @@ from controllers.productController import save, find_all
 
 product_blueprint = Blueprint("product_bp", __name__)
 
+
 product_blueprint.route('/', methods=['POST'])(save)
 product_blueprint.route('/', methods=['GET'])(find_all)
