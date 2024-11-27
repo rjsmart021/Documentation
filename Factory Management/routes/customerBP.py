889a3532c27d210428e602109d108eee301d4092
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-27 00:01:34 -0600

    Update customerBP.py

--- Factory Management/routes/customerBP.py
+++ Factory Management/routes/customerBP.py
@@ -3,5 +3,6 @@ from controllers.customerController import save, find_all
 
 customer_blueprint = Blueprint("customer_bp", __name__)
 
+
 customer_blueprint.route('/', methods=['POST'])(save)
 customer_blueprint.route('/', methods=['GET'])(find_all)
