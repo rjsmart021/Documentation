e8aa3f932b7771c081c3b1cf16b80c3d5934fd9c
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-27 00:01:21 -0600

    Update orderBP.py

--- Factory Management/routes/orderBP.py
+++ Factory Management/routes/orderBP.py
@@ -3,5 +3,6 @@ from controllers.orderController import save, find_all
 
 order_blueprint = Blueprint("order_bp", __name__)
 
+
 order_blueprint.route('/', methods=['POST'])(save)
 order_blueprint.route('/', methods=['GET'])(find_all)
